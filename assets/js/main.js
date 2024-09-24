d82525d5a8441cc3aa00d6c611b92e11261c56a5
Mahdieh2001 <112332619+Mahdieh2001@users.noreply.github.com>
2024-09-24 23:08:23 +0330

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,29 @@ const swiperHome = new Swiper('.home__swiper', {
     },
 
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        nextEl: '.swiper-button-next-sli',
+        prevEl: '.swiper-button-prev-sli'
     }
 })
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
